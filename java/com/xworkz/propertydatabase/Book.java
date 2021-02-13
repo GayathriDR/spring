36dc6b9a0ea2c8b0db26a1efeff1619dc422a891
GayathriDR <you@example.com>
2021-02-13 14:37:38 +0530

    databaseproperties

--- java/com/xworkz/propertydatabase/Book.java
+++ java/com/xworkz/propertydatabase/Book.java
@@ -1,30 +1,16 @@
 package com.xworkz.propertydatabase;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Book {
 	private String bookName;
 	private int bookId;
 	private double bookPrice;
-	public String getBookName() {
-		return bookName;
-	}
-	public void setBookName(String bookName) {
-		this.bookName = bookName;
-	}
-	public int getBookId() {
-		return bookId;
-	}
-	public void setBookId(int bookId) {
-		this.bookId = bookId;
-	}
-	public double getBookPrice() {
-		return bookPrice;
-	}
-	@Override
-	public String toString() {
-		return "Book [bookName=" + bookName + ", bookId=" + bookId + ", bookPrice=" + bookPrice + "]";
-	}
-	public void setBookPrice(double bookPrice) {
-		this.bookPrice = bookPrice;
-	}
-
+	  
+	
 }
--- java/com/xworkz/propertydatabase/DataManagerTester.java
+++ java/com/xworkz/propertydatabase/DataManagerTester.java
@@ -13,26 +13,43 @@ public class DataManagerTester {
 
 	public static void main(String[] args) {
 		ApplicationContext container=new ClassPathXmlApplicationContext("util.xml");
+		Book bb=container.getBean(Book.class);
+		String bName=bb.getBookName();
+		int bId=bb.getBookId();
+		double bPrice=bb.getBookPrice();
+		System.out.println(bId);
+		System.out.println(bb.getBookName());
 		DriverManagerDataSource driverConnection=container.getBean(DriverManagerDataSource.class);
 		try {
+			
 			Connection con=driverConnection.getConnection();
 			Statement stmt=con.createStatement();
 			String qry="insert into book values('web design',11,'500.00') ";
 			String qry1="select * from book";
-//			int record=stmt.executeUpdate(qry);
-//			System.out.println(record+"this many records got updated");
+			String qry2="insert into book values('"+bName+"',"+bId+",'"+bPrice+"')";
+			int record=stmt.executeUpdate(qry2);
+			System.out.println(record+" this many records got updated");
 		ResultSet rs=	stmt.executeQuery(qry1);
 		while(rs.next()){
-			System.out.println(rs.toString());
+//			System.out.println(rs.toString());
+			String bookName=rs.getString(1);
+			int bookId=rs.getInt(2);
+			double bookPrice=rs.getDouble(3);
+			System.out.println("book name:"+bookName+"   book id:"+bookId+"   book price:"+bookPrice);;
 			
 		}
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		//lombok tool
+		Book b=new Book();
+		b.setBookName("bootstrap");
+		b.setBookId(12);
+		b.setBookPrice(300.00);
+		b.getBookName();
 
 		
-//		System.out.println(driverConnection.getPassword());
 	}
 
 }
